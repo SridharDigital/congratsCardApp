43b9cc793d5e3551c0788406681b97163ed72853
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2022-02-10 16:42:19 +0000

    Ready to Submit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const element = (
-    // Write your code here.
-  )
-  
-ReactDOM.render(element, document.getElementById('root'))
+  <div className="bg-container">
+    <h1 className="main-heading">Congratulations</h1>
+    <div className="content-box">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h2 className="profile-name">Kiran V</h2>
+      <p className="description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
